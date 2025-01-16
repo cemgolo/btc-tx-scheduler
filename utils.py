d34f22a1f1c8818ad58a8395202f52e4ee56b264
cemgolo <cemalbal@ikmail.com>
2025-01-16 16:33:18 +0300

    Remove unnecessary blank lines

--- utils.py
+++ utils.py
@@ -5,8 +5,6 @@ import base58
 def get_current_time() -> datetime:
     return datetime.now()
 
-
-
 def public_key_to_address(public_key: bytes) -> str:
     sha256 = hashlib.sha256(public_key).digest()
     ripemd160 = hashlib.new('ripemd160', sha256).digest()
